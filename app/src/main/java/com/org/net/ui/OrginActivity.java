a9851d9c3b2fbe16189750655aff24db1e17ee66
guozhk <1565619659@qq.com>
2017-12-27 14:28:28 +0800

    modify originActivity

--- app/src/main/java/com/org/net/ui/OrginActivity.java
+++ app/src/main/java/com/org/net/ui/OrginActivity.java
@@ -2,12 +2,15 @@ package com.org.net.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.org.net.R;
 import com.org.net.base.BaseActivity;
 
 public class OrginActivity extends BaseActivity {
 
+    private TextView tvResult;
 
     @Override
     protected int getLayoutId() {
@@ -16,6 +19,18 @@ public class OrginActivity extends BaseActivity {
 
     @Override
     protected void initView() {
+        tvResult = queryViewById(R.id.nohttp_request_result);
+        queryViewById(R.id.nohttp_request_origin, true);
+    }
 
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.nohttp_request_origin:
+                break;
+            default:
+                break;
+        }
     }
 }
