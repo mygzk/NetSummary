adc8e5424f9f3b07b25bae1a388e1926eb4c7d90
guozhk <1565619659@qq.com>
2017-12-27 14:16:24 +0800

    project base framework

---
+++ app/src/main/java/com/org/net/base/BaseActivity.java
@@ -0,0 +1,64 @@
+package com.org.net.base;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by guozhk on 2017/12/14.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity implements View.OnClickListener {
+    String TAG = getClass().getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutId());
+        initView();
+    }
+
+
+    protected abstract int getLayoutId();
+
+    protected abstract void  initView();
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+
+    protected <T extends View> T queryViewById(int resId) {
+        return (T) findViewById(resId);
+
+    }
+
+    protected <T extends View> T queryViewById(View parentView, int resId) {
+        return (T) parentView.findViewById(resId);
+
+    }
+
+    protected <T extends View> T queryViewById(int viewId, boolean clickListener) {
+        if (viewId > 0) {
+            T view = (T) findViewById(viewId);
+            if (clickListener && view != null)
+                addOnClickListener(view);
+            return view;
+        }
+        return null;
+    }
+
+    protected void addOnClickListener(View... views) {
+        for (int i = 0; i < views.length; i++)
+            views[i].setOnClickListener(this);
+    }
+
+
+}
---
+++ app/src/main/java/com/org/net/ui/OrginActivity.java
@@ -0,0 +1,21 @@
+package com.org.net.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.org.net.R;
+import com.org.net.base.BaseActivity;
+
+public class OrginActivity extends BaseActivity {
+
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_orgin;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
