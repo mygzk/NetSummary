3f994fb22840d822fe48a201708df3ff0ead494a
guozhk <1565619659@qq.com>
2018-04-26 17:31:37 +0800

    "test"

--- rxandroid_simple/src/main/java/com/org/rxsimple/TestClass.java
+++ rxandroid_simple/src/main/java/com/org/rxsimple/TestClass.java
@@ -1,5 +1,6 @@
 package com.org.rxsimple;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -11,11 +12,14 @@ import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Action;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.Subject;
 
 /**
  * Created by guozhk on 2018/4/20.
@@ -246,8 +250,7 @@ public class TestClass {
     }
 
 
-
-    private void testFlatMap1(){
+    private void testFlatMap1() {
         Observable.create(new ObservableOnSubscribe<Integer>() {
             @Override
             public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
@@ -262,7 +265,7 @@ public class TestClass {
                 for (int i = 0; i < 3; i++) {
                     list.add("I am value " + integer);
                 }
-                return Observable.fromIterable(list).delay(10,TimeUnit.MILLISECONDS);
+                return Observable.fromIterable(list).delay(10, TimeUnit.MILLISECONDS);
             }
         }).subscribe(new Consumer<String>() {
             @Override
@@ -275,32 +278,155 @@ public class TestClass {
 
 
     /**
-     *
      * @param
      */
 
-    private void testDefer(){
-        Observable.defer(new Callable<ObservableSource<String>>() {
+    private void testDefer() {
+//        Observable.defer(new Callable<ObservableSource<String>>() {
+//            @Override
+//            public ObservableSource<String> call() throws Exception {
+//                String[] s= {"adad","a123"};
+//                return Observable.fromArray(s);
+//            }
+//        }).subscribe(new Consumer<String>() {
+//            @Override
+//            public void accept(String s) throws Exception {
+//                printer(s);
+//            }
+//        });
+
+
+        Integer[] items = {1, 2, 3, 4, 5};
+        Integer[] items1 = {1, 2, 3, 4, 5};
+        Integer[] items2 = {1, 2, 3, 4, 5};
+        Integer[] items3 = {1, 2, 3, 4, 5};
+        Integer[] items4 = {1, 2, 3, 4, 5};
+
+//        String s="s";
+//        Observable.fromArray(items).subscribe(new Consumer<Integer>() {
+//            @Override
+//            public void accept(Integer ints) throws Exception {
+//
+////                for (Integer i:ints){
+////                    printer(i+"");
+////                }
+//                printer(ints+"");
+//            }
+//        });
+
+//        Observable.just(s,items).subscribe(new Consumer<Serializable>() {
+//            @Override
+//            public void accept(Serializable serializable) throws Exception {
+//
+//            }
+//        });
+
+//        Observable.just(items).subscribe(new Consumer<int[]>() {
+//            @Override
+//            public void accept(int[] ints) throws Exception {
+//
+//                for (int i:ints){
+//                    printer(i+"");
+//                }
+//
+//            }
+//        });
+
+
+//        Observable.just(items).map(new Function<Integer[], String[]>() {
+//
+//            @Override
+//            public String[] apply(Integer[] integers) throws Exception {
+//                String[] strings ={"s","d","f"};
+//                return strings;
+//            }
+//        }).subscribe(new Consumer<String[]>() {
+//
+//            @Override
+//            public void accept(String[] strings) throws Exception {
+//                for (String i:strings){
+//                    printer(i+"");
+//                }
+//            }
+//        });
+
+
+//        Observable.timer(5, TimeUnit.SECONDS).subscribe(new Observer<Long>() {
+//            @Override
+//            public void onSubscribe(Disposable d) {
+//                printer("onSubscribe");
+//            }
+//
+//            @Override
+//            public void onNext(Long aLong) {
+//                printer("onNext");
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//                printer("onError");
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//                printer("onComplete");
+//            }
+//
+//
+//        });
+//
+
+        Observable.timer(3,TimeUnit.SECONDS).subscribe(new Consumer<Long>() {
             @Override
-            public ObservableSource<String> call() throws Exception {
-                String[] s= {"adad","a123"};
-                return Observable.fromArray(s);
-            }
-        }).subscribe(new Consumer<String>() {
-            @Override
-            public void accept(String s) throws Exception {
-                printer(s);
+            public void accept(Long aLong) throws Exception {
+                printer("onComplete");
             }
         });
-    }
 
+
+
+
+//        Observable.range(2,5).subscribe(new Consumer<Integer>() {
+//            @Override
+//            public void accept(Integer integer) throws Exception {
+//                printer("integer:"+integer);
+//            }
+//        });
+
+
+//        Observable.just(items,items2).elementAt(0).subscribe(new Observer<>());
+
+
+
+
+
+
+//        Observable.timer(2, TimeUnit.SECONDS)
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(Schedulers.newThread()) // timer 默认在新线程，所以需要切换回主线程
+//                .subscribe(new Consumer<Long>() {
+//                    @Override
+//                    public void accept(@NonNull Long aLong) throws Exception {
+//                        printer("integer:"+aLong);
+//                    }
+//                });
+
+        Observable.just(1,2,3,4,5,6)
+                .buffer(3,2)
+                .subscribe(new Consumer<List<Integer>>() {
+            @Override
+            public void accept(List<Integer> integers) throws Exception {
+                printer("integer:"+integers.size());
+            }
+        });
+
+
+    }
 
     public static void printer(String str) {
         System.out.println(str);
 
     }
-
-
 
 
 }
