332a2badb28ad11ec260d255ab57fdf31c1cbcd3
guozhk <1565619659@qq.com>
2018-11-09 17:41:04 +0800

    add ssl verifier

--- rxandroid_simple/src/main/java/com/org/rxsimple/net/RetrofitManager.java
+++ rxandroid_simple/src/main/java/com/org/rxsimple/net/RetrofitManager.java
@@ -2,13 +2,13 @@ package com.org.rxsimple.net;
 
 import android.os.Environment;
 
+import com.org.rxsimple.App;
+
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
-
 import okhttp3.Cache;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -45,6 +45,7 @@ public class RetrofitManager {
 
 
     private OkHttpClient getOkhttp() {
+
         File cacheFile = new File(Environment.getExternalStorageDirectory(), "netCache");
         Cache cache = new Cache(cacheFile, 10 * 1024 * 1024);//google建议放到这里
         return new OkHttpClient.Builder()
@@ -55,6 +56,9 @@ public class RetrofitManager {
                 .cache(cache)
                 .addInterceptor(new LoggingInterceptor())
                 .addNetworkInterceptor(new CacheInterceptor())
+                //暂时这样处理
+               // .sslSocketFactory(HttpsFactory.getSSLSocketFactory(App.getApp(), HttpConfig.certificates))
+                .hostnameVerifier(new HttpsHostnameVerifier(HttpConfig.BASE_URLS))
                 .build();
     }
 
